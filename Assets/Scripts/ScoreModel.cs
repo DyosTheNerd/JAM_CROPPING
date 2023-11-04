73ca818508ade771fe550a0e9c85f622c5adf135
Lievven <opti8298@gmx.de>
2023-11-04 16:48:25 +0100

    Implemented REST functionality; added RestClient & FullSerializer

---
+++ Assets/Scripts/ScoreModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class ScoreModel
+{
+    public string username = "Test";
+    public int score = 1000000;
+}
