72e37fafbf0d22e1a2a4c345f31da4bc7d49e58d
DyosTheNerd <irondwarf@gmx.de>
2023-11-04 11:32:20 +0100

    adding goal and ui

--- Assets/Scripts/Level/Goal/GoalManager.cs
+++ Assets/Scripts/Level/Goal/GoalManager.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class GoalManager : MonoBehaviour
 {
     public static GoalManager instance;
+
+    public Goal[] goals;
     
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        _setupGoals(132456);
+
     }
 
     public Color ScoreItemsAndDetermineColor(List<Item>  itemsToScore)
     {
+        
         return itemsToScore[0].GetColor();
-    } 
+    }
+
+    private void _setupGoals(int seed)
+    {
+        
+    }
+    
 }
